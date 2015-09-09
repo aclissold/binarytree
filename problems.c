c34273c153b11f59f2ba9de13912a0df9f4e8aff
Andrew Clissold <andrewclissold@gmail.com>
2015-09-08 20:24:24 -0400

    Solve problems 1.b and 1.c

--- problems.c
+++ problems.c
@@ -5,6 +5,7 @@
 /* problem 1 */
 struct node *build123()
 {
+	/* version a
 	struct node *one = new_node(1);
 	struct node *two = new_node(2);
 	struct node *three = new_node(3);
@@ -13,4 +14,21 @@ struct node *build123()
 	two->right = three;
 
 	return two;
+	*/
+
+	/* version b
+	struct node *tree = new_node(2);
+	tree->left = new_node(1);
+	tree->right = new_node(3);
+
+	return tree;
+	 */
+
+	/* version c */
+	struct node *tree;
+	tree = insert(tree, 2);
+	tree = insert(tree, 1);
+	tree = insert(tree, 3);
+
+	return tree;
 }
